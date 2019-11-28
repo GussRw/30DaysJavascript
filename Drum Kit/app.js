6ac0abc02686bbe37ffaf141ad38b254bc57cb8d
Guss Rw <gussrw1@gmail.com>
2019-11-27 22:01:14 -0600

    drum kit was completed

--- Drum Kit/app.js
+++ Drum Kit/app.js
@@ -1,6 +1,6 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
@@ -18,3 +18,29 @@ window.addEventListener('keydown', playSound);
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+let counter = 6;
+let time = 400;
+setTimeout(()=>{
+    setInterval(() => {
+        playSound({keyCode: 83});
+
+        if (counter % 4 == 0)
+            playSound({keyCode: 65});
+        if (counter % 2 == 0 && !(counter % 4 == 0))
+            playSound({keyCode: 71});
+        if (counter % 8 == 0)
+            playSound({keyCode: 68});
+
+        if (counter % 2 == 0 && counter % 8 == 0)
+            setTimeout(() => {
+                playSound({keyCode: 71});
+                setTimeout(() => {
+                    playSound({keyCode: 72});
+                }, time+time+time+(time/2))
+            }, time / 2)
+
+        counter++;
+    }, time)
+},5000);
+
